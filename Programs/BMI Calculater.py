61fa3ff1a929c36190c7838876445693b6f542bd
BarziBahadin <realberzy@gmail.com>
2022-10-13 01:45:58 +0300

    update BMI calculater

--- Programs/BMI Calculater.py
+++ Programs/BMI Calculater.py
@@ -2,6 +2,18 @@ import math
 hight = float(input("what is your  hight:")) # just normal asking for hight
 weight = int(input("what is your weight:"))    # and  
 
-bmi = weight / (hight**2) # callculate bmi
+bmi = math.floor(weight / (hight**2) )# callculate bmi
 # print to user 
-print(f"your bmi is {math.floor(bmi)}") 
+# with some information
+# ----------------------------------------------------------------
+
+if (bmi<18) :
+    print(f"Your bmi is {bmi} and under weights")
+elif (bmi>18 and bmi<25):
+    print(f"Your bmi is {bmi} and normal")
+elif (bmi>25 and bmi<30):
+    print(f"Your bmi is {bmi} and over weight")
+elif (bmi>30 and bmi<35):
+    print(f"Your bmi is {bmi} and obese")
+else:
+    print(f"Your bmi is {bmi} and clinically obese.")
---
+++ Programs/oddOrEveen.py
@@ -0,0 +1,6 @@
+number = int(input("what is your  number:"))
+
+if (number % 2 == 0):
+    print(f"your number {number} is even")
+else:
+    print(f"your number {number} is odd")
